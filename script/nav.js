96bde1aa8f555965466109d2c9c8fab025d935ba
Iqbal Ali <drp4586@gmail.com>
2022-03-30 23:09:09 +0530

    iq_nav_js

---
+++ script/nav.js
@@ -0,0 +1,163 @@
+const navbar = ()=>{
+    return `<div id="nmain">
+    <img src="https://internshala.com/static/images/common/new_internshala_logo.svg" alt="">
+    <div>
+        <div onmouseenter="showOption(0)" class="optionDiv">
+            <a href="">Internships </a>
+            <div class="icon"></div>
+        </div>
+        <div onmouseenter="showOption(1)" class="optionDiv">
+            <a href="">Online Trainings </a>
+            <div id="off">70% OFF</div>
+            <div class="icon"></div>
+        </div>
+        <div onmouseenter="showOption(2)" class="optionDiv">
+            <a href="">Jobs </a>
+            <div class="icon"></div>
+        </div>
+        <div>
+            <div id="nav_log">
+                <a href="">Login</a>
+            </div>
+        </div>
+        <div onmouseenter="showOption(3)" class="optionDiv">
+    
+            <div id="nav_raj">
+                <div>
+                    <a href="">Register </a>
+                    <div class="icon"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    
+    <div id="nav_option">
+    <div class="navOptiondiv">
+        <div>
+            <p>Location</p>
+            <p>Profile</p>
+            <p>Category</p>
+            <p>Specializations</p>
+        </div>
+        <div>
+            <a href="">Work from Home</a>
+            <a href="">Internship in India</a>
+            <a href="">Internship in Delhi</a>
+            <a href="">Internship in Bangalore</a>
+            <a href="">Internship in Hyderabad</a>
+            <a href="">Internship in Mumbai</a>
+            <a href="">Internship in Chennai</a>
+            <a href="">Internship in Gurgaon</a>
+            <a href="">Internship in Kolkata</a>
+            <a href="">Virtual internship</a>
+            <a href="">Work from Home</a>
+            <a href="">Internship in India</a>
+            <a href="">Internship in Delhi</a>
+            <a href="">Internship in Bangalore</a>
+            <a href="">Internship in Hyderabad</a>
+            <a href="">Internship in Mumbai</a>
+            <a href="">Internship in Chennai</a>
+            <a href="">Internship in Gurgaon</a>
+            <a href="">Internship in Kolkata</a>
+            <a href="">Virtual internship</a>
+    
+        </div>
+    </div>
+    <div class="navOptiondiv">
+        <div>
+            <p>Location</p>
+            <p>Profile</p>
+            <p>Category</p>
+            <p>Specializations</p>
+        </div>
+        <div>
+            <a href="">Work from Home</a>
+            <a href="">Internship in India</a>
+            <a href="">Internship in Delhi</a>
+            <a href="">Internship in Bangalore</a>
+            <a href="">Internship in Hyderabad</a>
+            <a href="">Internship in Mumbai</a>
+            <a href="">Internship in Chennai</a>
+            <a href="">Internship in Gurgaon</a>
+            <a href="">Internship in Kolkata</a>
+            <a href="">Virtual internship</a>
+            <a href="">Work from Home</a>
+            <a href="">Internship in India</a>
+            <a href="">Internship in Delhi</a>
+            <a href="">Internship in Bangalore</a>
+            <a href="">Internship in Hyderabad</a>
+            <a href="">Internship in Mumbai</a>
+            <a href="">Internship in Chennai</a>
+            <a href="">Internship in Gurgaon</a>
+            <a href="">Internship in Kolkata</a>
+            <a href="">Virtual internship</a>
+    
+        </div>
+    </div>
+    <div class="navOptiondiv">
+        <div>
+            <p>Location</p>
+            <p>Profile</p>
+            <p>Category</p>
+            <p>Specializations</p>
+        </div>
+        <div>
+            <a href="">Work from Home</a>
+            <a href="">Internship in India</a>
+            <a href="">Internship in Delhi</a>
+            <a href="">Internship in Bangalore</a>
+            <a href="">Internship in Hyderabad</a>
+            <a href="">Internship in Mumbai</a>
+            <a href="">Internship in Chennai</a>
+            <a href="">Internship in Gurgaon</a>
+            <a href="">Internship in Kolkata</a>
+            <a href="">Virtual internship</a>
+            <a href="">Work from Home</a>
+            <a href="">Internship in India</a>
+            <a href="">Internship in Delhi</a>
+            <a href="">Internship in Bangalore</a>
+            <a href="">Internship in Hyderabad</a>
+            <a href="">Internship in Mumbai</a>
+            <a href="">Internship in Chennai</a>
+            <a href="">Internship in Gurgaon</a>
+            <a href="">Internship in Kolkata</a>
+            <a href="">Virtual internship</a>
+    
+        </div>
+    </div>
+    <div class="navOptiondiv">
+        <div>
+            <p>Location</p>
+            <p>Profile</p>
+            <p>Category</p>
+            <p>Specializations</p>
+        </div>
+        <div>
+            <a href="">Work from Home</a>
+            <a href="">Internship in India</a>
+            <a href="">Internship in Delhi</a>
+            <a href="">Internship in Bangalore</a>
+            <a href="">Internship in Hyderabad</a>
+            <a href="">Internship in Mumbai</a>
+            <a href="">Internship in Chennai</a>
+            <a href="">Internship in Gurgaon</a>
+            <a href="">Internship in Kolkata</a>
+            <a href="">Virtual internship</a>
+            <a href="">Work from Home</a>
+            <a href="">Internship in India</a>
+            <a href="">Internship in Delhi</a>
+            <a href="">Internship in Bangalore</a>
+            <a href="">Internship in Hyderabad</a>
+            <a href="">Internship in Mumbai</a>
+            <a href="">Internship in Chennai</a>
+            <a href="">Internship in Gurgaon</a>
+            <a href="">Internship in Kolkata</a>
+            <a href="">Virtual internship</a>
+    
+        </div>
+    </div>
+    </div>
+    `
+    }
+    export default navbar
